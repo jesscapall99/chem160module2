4a0552847740451ff366385b7d38b70707d770ca
jesscapall99 <70401702+jesscapall99@users.noreply.github.com>
2020-09-06 20:49:02 -0700

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,11 @@
+import random, math
+inside = 0
+ntrials=10000
+for i in range(ntrials):
+    x=random.random()
+    y=random.random()
+    if (x*x+y*y)<1.0:
+        inside= inside + 1
+pi=4.*inside/ntrials
+print("N=%d Error=%8.5f "%(ntrials, pi-math.pi))
+
